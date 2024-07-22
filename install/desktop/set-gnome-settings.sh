dc4b8883dbf3ae40c6414ffe98dba745dd9c27a8
Loc Le <lpl212757@gmail.com>
2024-07-23 04:11:49 +0700

    feat - update shell theme and set gnome cursor as bibata

--- install/desktop/set-gnome-settings.sh
+++ install/desktop/set-gnome-settings.sh
@@ -6,3 +6,7 @@ gsettings set org.gnome.desktop.interface monospace-font-name 'CaskaydiaMono Ner
 
 # Reveal week numbers in the Gnome calendar
 gsettings set org.gnome.desktop.calendar show-weekdate true
+
+# Switch app in curent workspace only
+gsettings set org.gnome.shell.window-switcher current-workspace-only true
+gsettings set org.gnome.shell.app-switcher current-workspace-only true
