00fafebca2d5d57af13af15235d61854afa72533
Loc Le <lpl212757@gmail.com>
2024-08-24 16:32:34 +0700

    feat - update boot

--- boot.sh
+++ boot.sh
@@ -12,12 +12,11 @@ echo -e "$ascii_art"
 echo "=> Omakub is for fresh Ubuntu 24.04 installations only!"
 echo -e "\nBegin installation (or abort with ctrl+c)..."
 
-sudo apt-get update >/dev/null
-sudo apt-get install -y git >/dev/null
+sudo yay -Sy git >/dev/null
 
 echo "Cloning stable Omakub..."
 rm -rf ~/.local/share/omakub
-git clone -b stable https://github.com/basecamp/omakub.git ~/.local/share/omakub >/dev/null
+git clone -b stable https://github.com/locle97/arch-omakub.git ~/.local/share/omakub >/dev/null
 
 echo "Installation starting..."
 source ~/.local/share/omakub/install.sh
