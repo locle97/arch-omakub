9aeaf33a5748ad449a79d34b2a1d37cba5b631e0
Loc Le <lpl212757@gmail.com>
2024-08-31 15:59:48 +0700

    feat - install yay before install terminal

--- install/terminal.sh
+++ install/terminal.sh
@@ -1,5 +1,11 @@
+# Install yay first
+pacman -S --needed git base-devel
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+
 # Needed for all installers
-yay -Syu curl git unzip
+yay -Syu --needed --noconfirm  curl git unzip
 
 # Run terminal installers
 for installer in ~/.local/share/omakub/install/terminal/*.sh; do source $installer; done
