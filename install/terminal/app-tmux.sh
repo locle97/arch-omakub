0008bd268379f7a0c0d6bf869399f8cdfae75624
Loc Le <lpl212757@gmail.com>
2024-09-05 23:25:18 +0700

    Revert "Update app-tmux.sh"
    This reverts commit 1edb314a2facc2985110facd1eba807e6fdfe092.

--- install/terminal/app-tmux.sh
+++ install/terminal/app-tmux.sh
@@ -1,6 +1,6 @@
 yay -Sy --needed --noconfirm tmux
 
-rm -r ~/.tmux/plugins/tpm
+sudo rm -r ~/.tmux/plugins/tpm
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 cp ~/.local/share/omakub/configs/.tmux.conf ~/.tmux.conf
