e1e7c80a6fcc35e6c78338b9715641d8adeb3e26
Le Phat Loc <34123125+locle97@users.noreply.github.com>
2024-09-05 23:13:19 +0700

    Delete install/terminal/app-tmux.sh

--- install/terminal/app-tmux.sh
+++
@@ -1,6 +0,0 @@
-yay -Sy --needed --noconfirm tmux
-
-rm -r ~/.tmux/plugins/tpm
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-
-cp ~/.local/share/omakub/configs/.tmux.conf ~/.tmux.conf
