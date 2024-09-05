148ceb9527f73e411a294c8fe5f707fff6f4f86f
Loc Le <lpl212757@gmail.com>
2024-09-05 22:40:01 +0700

    Revert "feat - install yay before install terminal"
    This reverts commit 9aeaf33a5748ad449a79d34b2a1d37cba5b631e0.

--- install/terminal.sh
+++ install/terminal.sh
@@ -1,5 +1,5 @@
 # Needed for all installers
-yay -Syu --needed --noconfirm  curl git unzip
+yay -Syu curl git unzip
 
 # Run terminal installers
 for installer in ~/.local/share/omakub/install/terminal/*.sh; do source $installer; done
